55654446ff1443877cf987c20861d984d1dfe484
sevde23 <sevdebeyzakir@gmail.com>
2023-11-09 22:23:18 +0300

    Add project files.

---
+++ w01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w01
+{
+    class Program
+    {
+        //starting or entry point method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world"); //printf
+
+        }
+    }
+}
+//tokens:
+// -keywords
+// -identifiers
+// -symbols
+// -operators
+// -literals(numerical/string)
